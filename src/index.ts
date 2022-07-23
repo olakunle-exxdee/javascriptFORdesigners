71c2cbc5d8ede6aff8354ddc21ca207d1fbc6670
olakunle-exxdee <58233521+olakunle-exxdee@users.noreply.github.com>
2022-07-23 13:29:58 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,84 @@
+let pageNumber: number = 0;
+
+const pages = [
+  {
+    Background: "#edc7a9",
+    Circle: "#3e78ed",
+    Copy: "a Brooklyn-based graphic designer",
+  },
+  {
+    Background: "#a1fffe",
+    Circle: "#e34a47",
+    Copy: "Kanye West's biggest fan",
+  },
+  {
+    Background: "#d3c7f3",
+    Circle: "#f7fe00",
+    Copy: "looking for a job at the start of October",
+  },
+  {
+    Background: "#faffb8",
+    Circle: "#b472e6",
+    Copy: "letting you <a href='pdf.pdf'>download her PDF</a>",
+  },
+];
+
+const nextTag = document.querySelector("footer img.next");
+const prevTag = document.querySelector("footer img.previous");
+const randomTag = document.querySelector("footer img.random");
+const outputTag = document.querySelector("h2");
+const circleTag = document.getElementById("circles");
+const bodyTag = document.querySelector("body");
+
+const next = function () {
+  pageNumber++;
+  if (pageNumber > pages.length - 1) {
+    pageNumber = 0;
+  }
+  updateSection();
+};
+
+const updateSection = () => {
+  if (outputTag?.innerHTML) {
+    outputTag.innerHTML = pages[pageNumber].Copy;
+  }
+  if (circleTag) {
+    circleTag.style.backgroundColor = pages[pageNumber].Circle;
+  }
+  if (bodyTag) {
+    bodyTag.style.backgroundColor = pages[pageNumber].Background;
+  }
+};
+
+const previous = () => {
+  pageNumber--;
+  if (pageNumber < 0) {
+    pageNumber = pages.length - 1;
+  }
+  updateSection();
+};
+const random = () => {
+  pageNumber = Math.floor(Math.random() * pages.length);
+  updateSection();
+};
+
+nextTag?.addEventListener("click", () => {
+  next();
+});
+
+prevTag?.addEventListener("click", () => {
+  previous();
+});
+
+randomTag?.addEventListener("click", () => {
+  random();
+});
+
+document.addEventListener("keyup", (e) => {
+  if (e.key == "ArrowRight") {
+    next();
+  }
+  if (e.key == "ArrowLeft") {
+    previous();
+  }
+});
